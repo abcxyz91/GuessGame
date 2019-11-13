3d4910b46ebc94787b1923e99ef64765b71a60ed
abcxyz91 <57361630+abcxyz91@users.noreply.github.com>
2019-11-13 20:37:36 +0900

    Add files via upload

---
+++ GuessGame.py
@@ -0,0 +1,29 @@
+# This is a guess a number game.
+
+import random
+
+print("Hello! What is your name?")
+userName = input()
+print("Nice to meet you, " + userName+ ". Let's play a guess a number game, shall we?")
+secretNumber = random.randint(1, 20)
+print("I am thinking of a number between 1 and 20")
+
+# Ask the player to guess 5 times
+
+for guessTaken in range(1, 6):
+    print("Take a guess:")
+    try:
+        guess = int(input())
+        if guess > secretNumber:
+            print("Your guess is too high!")
+        elif guess < secretNumber:
+            print("You guess is too low!")
+        else:
+            break # Stop the loop if guess correctly
+    except ValueError:
+        print("Invalid input. Please try again!")
+
+if guess == secretNumber:
+    print("Congratulation, " + userName + ". You guess correctly in " + str(guessTaken) + " times!")
+else:
+    print("Out of guess. The number I was thinking of was " + str(secretNumber))
